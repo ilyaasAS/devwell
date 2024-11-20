6cb1e9626370919342074b9122a32cf6255de20b
ilyaasAS <ilyaas.95.jv@gmail.com>
2024-11-20 15:00:13 +0100

    Inscription incompléte

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Hashage du mot de passe
+            $hashedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hashedPassword);
+
+            // Sauvegarde dans la base de données
+            $userRepository->save($user, true);
+
+            // Redirige vers la page de login ou la page d'accueil après l'inscription
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
