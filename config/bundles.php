d1ba6738f74cd76c34aaa925464fe2407b426fbe
ilyaasAS <ilyaas.95.jv@gmail.com>
2024-11-21 09:41:42 +0100

    NavBar imcompléte

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
 ];
