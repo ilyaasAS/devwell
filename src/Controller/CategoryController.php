d442f402559c7ebc440ada000b6159746425d317
ilyaasAS <ilyaas.95.jv@gmail.com>
2024-11-25 11:28:05 +0100

    Gestion de catégories + Page catégorie pour les User

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -12,6 +12,34 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
+
+    // Afficher toutes les catégories pour l'utilisateur
+    #[Route('/categorie', name: 'app_category_user_index', methods: ['GET'])]
+    public function indexu(EntityManagerInterface $entityManager): Response
+    {
+        // Récupérer toutes les catégories
+        $categories = $entityManager->getRepository(Category::class)->findAll();
+
+        return $this->render('category/user_index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    // Afficher les détails d'une catégorie (produits associés)
+    #[Route('/categorie/{id}', name: 'app_category_user_show', methods: ['GET'])]
+    public function showu(Category $category): Response
+    {
+        // Vérification si la catégorie existe
+        if (!$category) {
+            throw $this->createNotFoundException('The category does not exist.');
+        }
+
+        return $this->render('category/user_show.html.twig', [
+            'category' => $category,
+        ]);
+    }
+
+
     // Liste des catégories
     #[Route('/categories', name: 'app_category_index', methods: ['GET'])]
     public function index(EntityManagerInterface $entityManager): Response
