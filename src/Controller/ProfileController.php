b3fe6f9e315516b3ed5084438e8eb9b3a4ea869f
ilyaasAS <ilyaas.95.jv@gmail.com>
2024-11-25 12:37:30 +0100

    Barre de recherche non fonctionnel

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Form\ProfileType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,6 +14,30 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class ProfileController extends AbstractController
 {
+
+    // Liste des produits avec une possibilité de recherche
+    #[Route('/products', name: 'app_product_index', methods: ['GET'])]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Récupérer la valeur de la recherche (si elle existe)
+        $search = $request->query->get('search', '');
+
+        // Construire la requête pour chercher des produits par nom ou description
+        $productsQuery = $entityManager->getRepository(Product::class)->createQueryBuilder('p')
+            ->where('p.name LIKE :search OR p.description LIKE :search')
+            ->setParameter('search', '%'.$search.'%')
+            ->getQuery();
+
+        // Exécuter la requête pour récupérer les produits filtrés
+        $products = $productsQuery->getResult();
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+
+
     #[Route('/profile', name: 'app_profile', methods: ['GET', 'POST'])]
     public function edit(
         Request $request,
