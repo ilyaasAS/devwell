090e90c6435ab0a6c0f3cacf925268d682bc7867
ilyaasAS <ilyaas.95.jv@gmail.com>
2024-11-27 10:14:38 +0100

    CSS V2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
+    "./templates/**/*.html.twig", // Inclure les templates Twig
+    "./src/**/*.js", // Inclure les fichiers JS/TS
+    "./public/**/*.html", // Inclure les fichiers HTML statiques si nécessaire
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary_dw: "#1A2A40",
+        secondary_dw: "#9EFCED",
+        tertiary_dw: "#F5F5F5",
+        text_dw: "#333333",
+      },
+    },
   },
   plugins: [],
-}
+};
+
