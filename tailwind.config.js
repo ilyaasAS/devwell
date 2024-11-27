990307cd0d4069f71b0cab82a1de4451fcbf6c8f
ilyaasAS <ilyaas.95.jv@gmail.com>
2024-11-27 13:57:32 +0100

    CSS V5

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,9 @@ module.exports = {
         text_dw: "#333333", // Couleur texte
       },
       boxShadow: {
-        'secondary': '0 4px 6px rgba(158, 252, 237, 0.3)', // Ombre personnalisée avec la couleur secondary_dw
+        'secondary': '0 10px 30px rgba(158, 252, 237, 0.4)', // Ombre personnalisée plus douce
+        'product': '0 10px 20px rgba(158, 252, 237, 0.2), 0 4px 6px rgba(0, 0, 0, 0.1)', // Ombre plus douce pour les produits
+        'hover': '0 15px 30px rgba(158, 252, 237, 0.5)', // Ombre au survol
       },
     },
   },
