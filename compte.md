071dcab7585c0eb5cd569fa246f030e4a506a345
ilyaasAS <ilyaas.95.jv@gmail.com>
2024-12-20 11:47:09 +0100

    mailer mail de comfirmation après l'inscription

---
+++ compte.md
@@ -0,0 +1,19 @@
+
+Admin
+
+ilyaas@gmail.com
+
+mdp: Ilyaas@15122003
+
+_____________________________
+
+user@gmail.com
+
+mdp: Ismail@2582012
+
+__________________________________
+
+user2@gmail.com
+user3@gmail.com
+
+mdp: 123456789
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class RegistrationController extends AbstractController
 {
@@ -17,7 +19,8 @@ class RegistrationController extends AbstractController
     public function register(
         Request $request,
         EntityManagerInterface $entityManager,
-        UserPasswordHasherInterface $passwordHasher
+        UserPasswordHasherInterface $passwordHasher,
+        MailerInterface $mailer // Ajout du service MailerInterface
     ): Response {
         $user = new User();
 
@@ -35,6 +38,17 @@ class RegistrationController extends AbstractController
             // Sauvegarde dans la base de données via l'EntityManager
             $entityManager->persist($user); // Prépare l'entité pour l'enregistrement
             $entityManager->flush(); // Exécute la requête SQL pour sauvegarder l'utilisateur
+
+            // Envoi d'un e-mail de confirmation
+            $email = (new Email())
+                ->from('no-reply@yourdomain.com') // Adresse d'expéditeur
+                ->to($user->getEmail()) // Adresse du nouvel utilisateur
+                ->subject('Bienvenue sur notre site !')
+                ->html($this->renderView('emails/registration_confirmation.html.twig', [
+                    'user' => $user,
+                ]));
+
+            $mailer->send($email); // Envoie l'e-mail
 
             // Redirige vers la page de connexion ou d'accueil après l'inscription
             return $this->redirectToRoute('app_login');
