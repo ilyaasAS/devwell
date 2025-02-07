7529ff29d1c1f915ba06c0b0a7df3a7d929c33dd
ilyaasAS <ilyaas.95.jv@gmail.com>
2025-02-07 09:15:19 +0100

    Back office sur Dashboard

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+// src/Controller/DashboardController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/admin/dashboard', name: 'admin_dashboard')]
+    public function index()
+    {
+        // Vérifier si l'utilisateur a bien le rôle 'ROLE_ADMIN'
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException('Accès refusé');
+        }
+
+        // Si l'utilisateur est admin, afficher la page du dashboard
+        return $this->render('admin/dashboard/dashboard.html.twig');
+    }
+}
