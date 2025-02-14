7d1d6c6675cbf6afd556dca9a5147899ed2f82fc
ilyaasAS <ilyaas.95.jv@gmail.com>
2025-02-14 10:09:10 +0100

    Pallatte graphique Prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,26 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'], // Définir la police par défaut comme Roboto
+      },
       colors: {
         primary_dw: "#1A2A40", // Couleur primaire
         secondary_dw: "#9EFCED", // Couleur secondaire
-        tertiary_dw: "#F5F5F5", // Couleur tertiaire
+        tertiary_dw: "#ffffff", // Couleur tertiaire
         text_dw: "#333333", // Couleur texte
+      },
+      textColor: {
+        DEFAULT: '#333333', // Applique text_dw comme couleur par défaut
+        black: '#333333',    // Remplace le noir par text_dw
+        gray: '#333333',     // Remplace le gris par text_dw
       },
       boxShadow: {
         'secondary': '0 10px 30px rgba(158, 252, 237, 0.4)', // Ombre personnalisée plus douce
-        'product': '0 10px 20px rgba(158, 252, 237, 0.2), 0 4px 6px rgba(0, 0, 0, 0.1)', // Ombre plus douce pour les produits
+        'product': '0 10px 20px rgba(158, 252, 237, 0.2), 0 4px 6px rgba(158, 252, 237, 0.1)', // Ombre plus douce pour les produits
         'hover': '0 15px 30px rgba(158, 252, 237, 0.5)', // Ombre au survol
       },
     },
   },
   plugins: [],
-};
+}
